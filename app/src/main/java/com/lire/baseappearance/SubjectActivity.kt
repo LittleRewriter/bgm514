00d11cd0a38b75234403e66f9aa05eddd739e19a
hlq153@163.com <hlq153@163.com>
2021-05-18 18:45:14 +0800

    设置了Subject部分的布局

---
+++ app/src/main/java/com/lire/baseappearance/SubjectActivity.kt
@@ -0,0 +1,20 @@
+package com.lire.baseappearance
+
+import android.os.Bundle
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+
+class SubjectActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_subject)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                    .setAction("Action", null).show()
+        }
+    }
+}
